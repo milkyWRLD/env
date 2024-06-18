81e88f66fd95c561d9874cc595a61b0377ec275c
DEV ARTSID <you@example.com>
2024-06-18 20:49:02 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import "./css/style.css";
+import './css/style.css'
+import goblinImg from './img/goblin.png'
 
-import "./js/app";
+document.addEventListener('DOMContentLoaded', () => {
+	const gameBoard = document.createElement('div')
+	gameBoard.id = 'game-board'
+	gameBoard.className = 'game-board'
+	document.body.appendChild(gameBoard)
 
-// TODO: write your code in app.js
+	for (let i = 0; i < 16; i++) {
+		const cell = document.createElement('div')
+		gameBoard.appendChild(cell)
+	}
+
+	const goblin = document.createElement('img')
+	goblin.src = goblinImg
+	goblin.classList.add('goblin')
+
+	let currentCellIndex = Math.floor(Math.random() * 16)
+	gameBoard.children[currentCellIndex].appendChild(goblin)
+
+	setInterval(() => {
+		let newCellIndex
+		do {
+			newCellIndex = Math.floor(Math.random() * 16)
+		} while (newCellIndex === currentCellIndex)
+
+		gameBoard.children[newCellIndex].appendChild(goblin)
+		currentCellIndex = newCellIndex
+	}, 1000)
+})
